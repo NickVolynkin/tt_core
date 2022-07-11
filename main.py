d76d122ce652e5ea976ce42e014b8b65d51134e6
Sergey Vorontsov <piligrim@rootnix.net>
2022-07-11 16:10:24 +0300

    Start project

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#core math operation
+
+from tkinter import Y
+
+
+def sum(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
